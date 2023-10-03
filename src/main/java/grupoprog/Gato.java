60c5ad40ace1520892f424da4e1cbb9b1d684247
MarianaSardo <linkerita@gmail.com>
2023-10-03 11:07:42 -0300

    cree clase gato

---
+++ src/main/java/grupoprog/Gato.java
@@ -0,0 +1,24 @@
+package grupoprog;
+
+public class Gato {
+	
+
+    private String nombre;
+    private int edad;
+    private double peso;
+    private String ubicacion;
+    
+    
+	public Gato(String nombre, int edad, double peso, String ubicacion) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+		this.ubicacion = ubicacion;
+	}
+	
+	public Gato() {
+		
+	}
+
+}
